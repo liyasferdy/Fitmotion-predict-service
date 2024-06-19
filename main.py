941c40e1c4d07d0673f83e26b31586236fb66bf8
liyasferdy <liyastarigan011@gmail.com>
2024-06-19 19:23:02 +0700

    update : models update

--- main.py
+++ main.py
@@ -1,79 +1,119 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, HTTPException
 from google.cloud import storage
 from google.auth import exceptions as google_exceptions
-from fastapi.responses import StreamingResponse
 from io import BytesIO
 import pandas as pd
-from numpy import array, argmax, unique
-import onnxruntime as ort
+import numpy as np
+import onnxruntime as rt
 from time import time
 import os
+from scipy.signal import butter, lfilter
 
 # Initialize the FastAPI app
 app = FastAPI()
 
 # Set up Google Cloud Storage client
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/root/fitmotion/Fitmotion-predict-service/service-account-key.json"  # Update the path
 client = storage.Client()
-# bucket_name = "fitmotiion-imu-log"
 bucket_name = "fitmotion-imu-sensor"
 bucket = client.bucket(bucket_name)
 
 # Constants for the prediction model
 WINDOW_LENGTH = 150
 STRIDE_LENGTH = 10
-LABELS = ['dws', 'jog', 'sit', 'std', 'ups', 'wlk']
+LABELS = ['sit', 'dws', 'std', 'ups', 'wlk']
 
-# Function to generate sequences from the data
-def sequence_generator(x, length, stride):
-    seq_x = []
-    data_length = len(x)
+MAPPED_FEATURES = ['gravityx', 'gravityy', 'gravityz', 'useraccelerationx', 'useraccelerationy', 'useraccelerationz']
+FEATURES = ['Gravity X', 'Gravity Y', 'Gravity Z', 'User Acceleration X', 'User Acceleration Y', 'User Acceleration Z']
 
-    for i in range(0, data_length - length + 1, stride):
-        input_sequence = x.iloc[i : i + length]
-        seq_x.append(input_sequence)
-    return array(seq_x).astype('float32')
+FS = 200
+ORDER = 2
+CUTOFF = 30
+LOOKBACK = 20
 
-# Function to classify activities using the ONNX model
-async def classify(b_file: bytes, runtime=ort.InferenceSession('fitmotion_model.onnx')):
-    start = time()
-    csv_file = BytesIO(b_file)
-    df = pd.read_csv(csv_file)
-    df = df.drop(labels=['Unnamed: 0'], axis=1)
-    feat = sequence_generator(df, WINDOW_LENGTH, STRIDE_LENGTH)
+# Filter data with lowpass-filter butterworth
+def butter_filter(data, cutoff, fs, order=5, filter_type='low'):
+    nyq = 0.5 * fs
+    if isinstance(cutoff, list):
+        normal_cutoff = [c / nyq for c in cutoff]
+    else:
+        normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype=filter_type, analog=False)
+    y = lfilter(b, a, data)
+    return y
+
+# Preprocess data
+def preproc(data):
+    transformed = []
+    df = data[FEATURES]
     
-    # predictor
-    input_name = runtime.get_inputs()[0].name
-    output_name = runtime.get_outputs()[0].name
-    y_pred = runtime.run([output_name], {input_name: feat})[0]
-    y_pred = argmax(y_pred, axis=1)
+    buffer = [df.iloc[0].values]
+    for i in range(1, len(df)):
+        if not np.array_equal(df.iloc[i].values, df.iloc[i-1].values):
+            buffer.append(df.iloc[i].values)
+    buffer = np.array(buffer)
+
+    df_buff = pd.DataFrame(buffer, columns=FEATURES)
+
+    for column in FEATURES:
+        if 'User Acceleration' in column:
+            df_buff[column] = butter_filter(df_buff[column], CUTOFF, FS, ORDER)
+
+        for i in range(LOOKBACK):
+            df_buff[f'{column} (t-{i+1})'] = df_buff[column].shift(i+1)
+
+    df_buff_clean = df_buff.dropna().reset_index(drop=True)
+    transformed.append(df_buff_clean)
+
+    df_cleaned = pd.concat(transformed, ignore_index=True)
+    return df_cleaned
+
+# Load the model
+model_path = "fitmotion_model_rfc.onnx"  # Update the path
+sess = rt.InferenceSession(model_path)
+input_name = sess.get_inputs()[0].name
+label_name = sess.get_outputs()[0].name
+
+# Perform classification
+async def classify(file_content):
+    df = pd.read_csv(BytesIO(file_content))
+    cleaned = preproc(df)
     
-    val, count = unique(y_pred, return_counts=True)
-    index = val[argmax(count)]
-    end = (time() - start) * 1000
-    return LABELS[index], round(end, 2)
+    if cleaned.empty:
+        raise ValueError("No valid data after preprocessing")
 
-#get data from storage
-# @app.get("/download-csv/{filename}")
-# async def download_csv(filename: str):
-#     if not filename.endswith(".csv"):
-#         raise HTTPException(status_code=400, detail="Only .csv files are allowed.")
-    
-#     try:
-#         blob = bucket.blob(filename)
-#         if not blob.exists():
-#             raise HTTPException(status_code=404, detail="File not found.")
-        
-#         stream = BytesIO()
-#         blob.download_to_file(stream)
-#         stream.seek(0)
-#         return StreamingResponse(stream, media_type="text/csv", headers={"Content-Disposition": f"attachment; filename={filename}"})
-#     except google_exceptions.GoogleAuthError as e:
-#         raise HTTPException(status_code=500, detail="Authentication error with Google Cloud.") from e
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+    start_time = time()
+    predictions = sess.run([label_name], {input_name: cleaned.to_numpy()})
+    time_taken = (time() - start_time) * 1000
 
+    y_pred = predictions[0]
 
-#predict data
+    ranged = {
+        'sit': 0,
+        'dws': 0,
+        'std': 0,
+        'ups': 0,
+        'wlk': 0
+    }
+
+    ranged_calc = {
+        'sit': 0,
+        'dws': 0,
+        'std': 0,
+        'ups': 0,
+        'wlk': 0
+    }
+
+    for data in y_pred:
+        label = LABELS[data]
+        ranged[label] += 1
+
+    for item in ranged:
+        ranged_calc[item] = ranged[item] / (15 * 60)
+
+    return ranged_calc, time_taken
+
+# Predict data
 @app.get("/predict-csv/{filename}")
 async def predict_csv(filename: str):
     if not filename.endswith(".csv"):
@@ -90,8 +130,8 @@ async def predict_csv(filename: str):
         b_file = stream.read()
 
         # Perform the prediction
-        label, time_taken = await classify(b_file)
-        return {"label": label, "time_taken_ms": time_taken}
+        ranged_calc, time_taken = await classify(b_file)
+        return {"ranged_calc": ranged_calc, "time_taken_ms": time_taken}
     except google_exceptions.GoogleAuthError as e:
         raise HTTPException(status_code=500, detail="Authentication error with Google Cloud.") from e
     except Exception as e:
